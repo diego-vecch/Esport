1a776375309c1e0ba4fa5711bfd3447e83dd713a
Diego <josediegoferia@gmail.com>
2023-11-23 18:00:06 -0300

    fix: add render host

--- Esport/settings.py
+++ Esport/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['vercel.app', '127.0.0.1', 'localhost'. 'https://blog-esport-first-devathon.onrender.com']
 
 
 # Application definition
